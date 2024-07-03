511552205d634f3da8d509a2c09bd8a708204a80
Roman Zalomsky <romanbusiness11@gmail.com>
2024-07-04 00:27:06 +0300

    delete useless imports

--- app/src/main/java/com/zalomsky/rickandmorty/features/characters/details/DetailsCharacterViewModel.kt
+++ app/src/main/java/com/zalomsky/rickandmorty/features/characters/details/DetailsCharacterViewModel.kt
@@ -2,18 +2,14 @@ package com.zalomsky.rickandmorty.features.characters.details
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.zalomsky.rickandmorty.domain.model.Character
 import com.zalomsky.rickandmorty.domain.usecase.characters.GetCharacterByIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
